750e674a2655936b51f28fcfbf8fce075e97d66c
Manuel Diaz <manueldiaz@ProdeManuelDiaz.home>
2017-10-11 20:43:27 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="es-es">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<title>Carto Test - Manuel Díaz</title>
+
+<!-- INSERT LINKS -->
+<?php 
+    require_once ('/home/mdv31226572/public_html/carto/include/links.html');
+?>
+
+</head>
+    <body class="body-blue uk-text-center">
+    	<!-- LOGO IMAGE-->
+    	<div class="logo-index-margin">
+    		<img src="images/blanco.svg" width="300px"/>
+    	</div>
+
+    	<!-- TITLE-->
+		<div class="vertical-align">
+        <h1 class="white-text raleway large uk-text-center">
+            <a class="x-large" href="documentation.php">Intro Carto test</a>
+        </h1>
+    </div>
+
+
+
+    </body>
+</html>
